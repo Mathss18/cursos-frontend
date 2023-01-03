67f1d87b2cbaf9ca21fd490b2f21fce5c312173d
Matheus Filho <45442720+Mathss18@users.noreply.github.com>
2023-01-03 02:16:42 -0300

    ip change

--- src/Service/api.ts
+++ src/Service/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api = axios.create({
     // baseURL: process.env.REACT_APP_API_URL,
-    baseURL: "http://127.0.0.1/api"
+    baseURL: "http://191.101.78.91:3000/api"
 });
 
 api.interceptors.request.use((config) => {
